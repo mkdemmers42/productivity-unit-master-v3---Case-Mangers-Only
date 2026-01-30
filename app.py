11b1f234b82b9e71cf55a64aa2ec2a6c79a13c0b
mkdemmers42 <mkdemmers42@gmail.com>
2026-01-29 22:27:21 -0800

    Update app.py

--- app.py
+++ app.py
@@ -372,8 +372,8 @@ file_key = f"uploaded_file_{k}"
 
 hours = st.text_input("Please insert **Hours Worked**", placeholder="Example: 148.13", key=hours_key)
 
-st.markdown("")
-st.markdown("")
+st.write("")
+st.write("")
 
 uploaded = st.file_uploader(
     'Upload the Excel (.xlsx) file exported from "Staff Service Detail Report" from SmartCare',
